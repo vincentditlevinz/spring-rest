956e699f3edb8a44389bf8f44f32c0ca83301145
vinz <vincentditlevinz@gmail.com>
2022-05-03 10:00:22 +0200

    provide data to react app and enable CORS

--- src/main/java/com/example/springrest/GreetingController.java
+++ src/main/java/com/example/springrest/GreetingController.java
@@ -1,12 +1,15 @@
 package com.example.springrest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
@@ -15,5 +18,10 @@ public class GreetingController {
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/notes")
+    public List<Note> findNotes() {
+        return List.of(new Note(1, 1, "test1", "lorem ipsum 1"), new Note(2, 2, "test2", "lorem ipsum 2"));
     }
 }
---
+++ src/main/java/com/example/springrest/Note.java
@@ -0,0 +1,32 @@
+package com.example.springrest;
+
+public class Note {
+    private final long userId;
+    private final long id;
+    private final String title;
+    private final String body;
+
+    public Note(long userId, long id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+}
